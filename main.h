1b6f292120a6eb9cb041c061fb3ab542d0f270b3
Nadira3 <paitanun35@gmail.com>
2023-05-19 16:15:42 +0000

    main.h update

--- main.h
+++ main.h
@@ -8,7 +8,13 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 #define BUFSIZE 1024
-char *skipSpaces(char *str);
+
+typedef struct var
+{
+	const char *str;
+	char (*func_ptr)(char *arg);
+} var_func;
+char *skip_spaces(char *str);
 char **parse_input(char *user_input);
 char *read_input(void);
 char *_strcat(char *str1, char *str2);
@@ -16,6 +22,7 @@ char *_strcpyp(char *str1, char *str2);
 char *_strcpy(char *str1, char *str2);
 int wordlen(char *str);
 int _strlen(char *str);
-int numWords(char *str);
+int num_words(char *str);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char (*interpret_func(char *arg_command))(char *arg);
 #endif
