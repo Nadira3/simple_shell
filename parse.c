ed5c54810e3259a1c7dc67ae66eccf6bcf29f382
Nadira3 <paitanun35@gmail.com>
2023-05-20 06:18:55 -0700

    file handling implemented in parse_input function

--- parse.c
+++ parse.c
@@ -1,4 +1,50 @@
 #include "main.h"
+
+char *path(char *command)
+{
+	char *filepath = getenv("PATH");
+	char *env_path = NULL;
+	size_t env_path_len;
+
+	char *path_copy = _strdup(filepath);
+
+	char *token = _strtok(path_copy, ":");
+	while (token != NULL)
+	{
+		if (*command == '/')
+		{
+			free(path_copy);
+			return command;
+		}
+
+		env_path_len = _strlen(token) + _strlen(command) + 2;
+		env_path = malloc(env_path_len);
+		if (env_path == NULL)
+		{
+			free(path_copy);
+			return NULL;
+		}
+
+		_strcpy(env_path, token);
+		_strcat(env_path, "/");
+		_strcat(env_path, command);
+
+		if (access(env_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return env_path;
+		}
+
+		free(env_path);
+		env_path = NULL;
+
+		token = _strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return env_path;
+}
+
 /**
  * parse_input - tokenizes a string
  * @user_input: string
@@ -6,7 +52,7 @@
  */
 char **parse_input(char *user_input)
 {
-	char **user_input_array = NULL, *start_to_write = user_input, *file_path = "/bin/";
+	char **user_input_array = NULL, *start_to_write = user_input;
 	int i = 0, j, len;
 
 	user_input_array = malloc(sizeof(char *) * (num_words(user_input) + 1));
@@ -16,9 +62,9 @@ char **parse_input(char *user_input)
 	{ 
 		start_to_write = skip_spaces(start_to_write);
 		if (*start_to_write == '\0')
-                    break;
+			break;
 		len = wordlen(start_to_write);
-		user_input_array[i] = i == 0 ? malloc(len + 6) : malloc(len + 1); /* find the executable */
+		user_input_array[i] = malloc(len + 1); 
 		if (!user_input_array[i])
 		{
 			for (j = i; j > 0; j--)
@@ -26,13 +72,7 @@ char **parse_input(char *user_input)
 			free(user_input_array);
 			return (0);
 		}
-		if (i == 0)
-		{
-			_strcpyp(user_input_array[i], file_path);
-			_strcat(user_input_array[i], start_to_write);
-		}
-		else
-			_strcpyp(user_input_array[i], start_to_write); 
+		_strcpyp(user_input_array[i], start_to_write); 
 		start_to_write += len;
 		i++;
 	}
